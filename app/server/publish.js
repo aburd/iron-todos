45593e598d671642225c3e799020d4191e341cac
Joe Burdick <josephdburdick@gmail.com>
2015-03-15 20:34:48 -0400

    Secure data if user not logged in.

--- app/server/publish.js
+++ app/server/publish.js
@@ -6,10 +6,14 @@
 
 
 Meteor.publish('todos', function (/* args */) {
+  if (!this.userId) return this.ready(); //Prevent data if user isn't logged in
+
   return Todos.find({userId: this.userId});
 });
 
 Meteor.publish('todo', function (id) {
+  if (!this.userId) return this.ready(); //Prevent data if user isn't logged in
+
 	var todo = Todos.findOne({_id: id});
   return [
   	Meteor.users.find({_id: todo.userId}, {fields: { profile: 1 }}),
@@ -19,10 +23,14 @@ Meteor.publish('todo', function (id) {
 });
 
 Meteor.publish('users', function (/* args */) {
+  if (!this.userId) return this.ready(); //Prevent data if user isn't logged in
+
   return Meteor.users.find({}, {fields: { profile: 1 }});
 });
 
 Meteor.publish('user', function (userId) {
+  if (!this.userId) return this.ready(); //Prevent data if user isn't logged in
+  
   return [
   	Meteor.users.find({_id: userId}, {fields: { profile: 1 }}),
   	Todos.find({userId: userId})
